e82ceb3772913f105f049fb123f63b2e3a3fa988
G. Mani Hrolfsson <mani@tungli.net>
2024-11-25 15:10:25 +0000

    Add UDH fields again that were removed in dfa42de3e85b83f72d2943b9c35f084c24224b1c

--- smpp/pdu/types.go
+++ smpp/pdu/types.go
@@ -308,6 +308,8 @@ func newDeliverSM(hdr *Header) *codec {
 			pdufield.DataCoding,
 			pdufield.SMDefaultMsgID,
 			pdufield.SMLength,
+			pdufield.UDHLength,
+			pdufield.GSMUserData,
 			pdufield.ShortMessage,
 		},
 	}
